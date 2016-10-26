66bf90f9aade509a0bd0ca6a903db48fafe7cd89
Roberta Doyle <ro.scalabrin@gmail.com>
2016-10-25 20:47:21 -0600

    add seed file for tags

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,33 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+class Seed
+  def self.start
+    create_tags
+  end
+
+  def self.create_tags
+    tags = ["climbing", 
+            "bouldering", 
+            "sport", 
+            "trad", 
+            "mini", 
+            "boards", 
+            "system", 
+            "hang", 
+            "campus", 
+            "thread", 
+            "peg", 
+            "fitness", 
+            "stretching", 
+            "cardio", 
+            "calisthenics", 
+            "TRX", 
+            "weights", 
+            "skills", 
+            "technical", 
+            "mental"]
+    tags.each do |tag|
+      Tag.create!(name: tag)
+    end
+  end
+end
+
+Seed.start
