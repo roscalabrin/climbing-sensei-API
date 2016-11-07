908224c084501844f3e27c9666a829aa293463f2
Roberta Doyle <ro.scalabrin@gmail.com>
2016-11-06 18:03:39 -0700

    edit session serializer

--- app/serializers/api/v1/session_serializer.rb
+++ app/serializers/api/v1/session_serializer.rb
@@ -1,12 +1,8 @@
 class Api::V1::SessionSerializer < ActiveModel::Serializer
-  attributes :email, :token_type, :user_id, :access_token
+  attributes :email, :user_id, :access_token
 
   def user_id
     object.id
-  end
-
-  def token_type
-    'Bearer'
   end
 
 end
